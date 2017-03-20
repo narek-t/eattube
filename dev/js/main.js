a2d0c97b36bbf04daf59d3ba8096994d2efdfaaa
Narek <narek.tarverdyan@bk.ru>
2017-03-20 16:08:38 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+function reScale(){
+	var scale = $(window).innerWidth() / 1920;
+	$(".scaleable-wrapper__inner").css({
+		transform: "scale(" + scale + ")"
+	});
+}
+
+$(document).ready(function () {
+	reScale();
+	$(window).on('resizeend', 1, function() {
+		reScale();
+	});
+	$('.toggle-menu').click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+		$('.main-nav').toggleClass('visible');
+	});
+	var header = $(".site-header");
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if (scroll >= 140) {
+			header.addClass("dark__header");
+		} else {
+			header.removeClass("dark__header");
+		}
+	});
+});
