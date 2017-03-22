1672cb46487f3949fe9262e6d32c48c76bfd0dd1
Narek <narek.tarverdyan@bk.ru>
2017-03-22 13:55:08 +0300

    last block

--- dev/js/main.js
+++ dev/js/main.js
@@ -27,4 +27,24 @@ $(document).ready(function () {
 	$('.tastes__input').iLightInputNumber({
 		mobile: false
 	});
+
+	var myVideo = document.getElementById("video_p");
+    $(".play__video").addClass("active_btn");
+    function playPause() {
+        if (myVideo.paused){
+            myVideo.play();
+            $(".play__video").addClass("active_btn");
+        } else {
+            myVideo.pause();
+            $(".play__video").removeClass("active_btn");
+        }
+    }
+    myVideo.onended = function() {
+    	$(".play__video").removeClass("active_btn op_btn");
+	};
+    $(".play__video").on('click', function(){
+        $(this).toggleClass("op_btn");
+        playPause();
+        return false
+    });
 });
